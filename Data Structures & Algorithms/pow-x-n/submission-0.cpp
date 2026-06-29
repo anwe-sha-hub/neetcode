ba8f0844eeed5a64d778959cc867dea3a1bf199d
ANWESHA JHA <anwesshajhaa@gmail.com>
2026-06-29 23:05:09 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+              long long N = n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        double ans = 1;
+        while (N > 0) {
+            if (N % 2 == 0) {
+                N /= 2;
+                x = x * x;
+            } else {
+                N = N - 1;
+                ans = ans * x;
+            }
+        }
+
+        return ans;
+    }
+    };
