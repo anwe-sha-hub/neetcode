69b765509be2e0636ea3b1bc6969840907fb873b
ANWESHA JHA <anwesshajhaa@gmail.com>
2026-09-13 21:17:36 +0530

    Add: max-consecutive-ones-iii - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones-iii/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+    int l=0,r=0,z=0,maxlen=0,len=0;
+
+    while(r<nums.size()){
+        if(nums[r]==0) z++;
+        if(z>k){
+            if(nums[l]==0) z--;
+            l++;
+        }
+        if(z<=k){
+            len=r-l+1;
+            maxlen=max(maxlen,len);
+        }
+        r++;
+    }
+    return maxlen;
+    }
+};
